14f96405ce65736d633965a6608efc62e7831fac
swsok <swsok@etri.re.kr>
2025-11-26 04:25:31 +0000

    make-increment-file buffer size to be 4kB instead of total file size

--- make-increment-file.c
+++ make-increment-file.c
@@ -9,50 +9,50 @@
 int main(int argc, char **argv)
 {
     int fd;
-    unsigned int *buffer;
+    unsigned int buffer[1024];  /* 4KB buffer (1024 * 4 bytes) */
     size_t size;
-    size_t i;
-    ssize_t nwrite;
     size_t total_written = 0;
+    size_t chunk_size;
+    size_t i;
+    unsigned int counter = 0;
+    ssize_t nwrite;
 
     if (argc < 3) {
-	printf("Usage: %s [file] [size]\n", argv[0]);
-	return 0;
+    	printf("Usage: %s [file] [size]\n", argv[0]);
+	    return 0;
     }
 
     fd = open(argv[1], O_CREAT | O_RDWR | O_TRUNC, 0666);
 
     if (fd < 0) {
-	printf("file open error - %s, errno=%d\n", argv[1], errno);
-	return 0;
+    	printf("file open error - %s, errno=%d\n", argv[1], errno);
+	    return 0;
     }
 
     size = atoll(argv[2]);
     printf("size = %zu\n", size);
 
-    buffer = (unsigned int *) malloc(size);
-    if (!buffer) {
-	printf("malloc failed for size %zu\n", size);
-	close(fd);
-	return 1;
-    }
-
-    for (i = 0; i < size / sizeof(unsigned int); i++) {
-	buffer[i] = (unsigned int)i;
-    }
-
     while (total_written < size) {
-	nwrite = write(fd, (char *)buffer + total_written, size - total_written);
-	if (nwrite < 0) {
-	    printf("write error, errno=%d\n", errno);
-	    break;
-	}
-	total_written += nwrite;
+	    chunk_size = size - total_written;
+	    if (chunk_size > sizeof(buffer))
+	        chunk_size = sizeof(buffer);
+
+	    for (i = 0; i < chunk_size / sizeof(unsigned int); i++) {
+	        buffer[i] = counter++;
+	    }
+
+	    nwrite = write(fd, buffer, chunk_size);
+	    if (nwrite < 0) {
+	        printf("write error, errno=%d\n", errno);
+	        break;
+    	}
+
+	    total_written += nwrite;
     }
 
     printf("written %zu bytes\n", total_written);
-    free(buffer);
     close(fd);
 
     return 0;
 }
+
